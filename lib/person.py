c8faec292ce413a1b221a60fe6081d264d3626b7
odongo <saonarawestinc@outlook.com>
2024-06-10 07:42:57 +0300

    completed instance methods lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+person1 = Person()
+person1.talk()
+person1.walk()
